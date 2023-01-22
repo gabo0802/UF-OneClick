ff13f785b08aea81c53dfdf4e80bf48ac7f00b17
Vladimir Alekseev <58440254+valekseev03@users.noreply.github.com>
2023-01-22 18:08:09 -0500

    SQL Connection Stuff (may or may not work)

--- MySQLStuff.go
+++ MySQLStuff.go
@@ -2,6 +2,7 @@ package main
 
 /*
 //Start of Back-End
+//Might Work (haven't tested yet since can't get driver to work yet)
 
 import (
 	"database/sql"
@@ -15,16 +16,37 @@ import (
 var db *sql.DB
 
 
+function getDatabaseSize(string database)(int64){
+	rows, err := db.Query("SELECT * FROM ?", database)
+	size := 0
+
+	for rows.Next(){
+		size := size + 1
+	}
+
+	return size
+}
+
 func SetUpDatabase(){
-	//see CREATE TABLES
+	//Users
+	db.Exec("CREATE TABLE IF NOT EXISTS Users (UserID int NOT NULL AUTO_INCREMENT, Username varchar(255) NOT NULL, Password varchar(255) NOT NULL, UNIQUE(Username), PRIMARY KEY(UserID));")
+
+	//Subscriptions
+	db.Exec("CREATE TABLE IF NOT EXISTS Subscriptions(UserID int NOT NULL, FOREIGN KEY(UserID) REFERENCES Users(UserID));")
 }
 
-func CreateNewUser(){
-	//Insert INTO Users _______________
+func CreateNewUser(username string, password string){
+	//Create New User
+	db.Exec("INSERT INTO Users(Username, Password) VALUES (?,?)", username, password);
+
+	//Test If User Creation Worked
 }
 
-func Login(){
-	//see Select ID FROM Users WHERE _________
+func Login(username string, password string){
+	//Try To Login
+	db.Query("SELECT ID FROM Users WHERE Username = ? AND Password = ?", username, password);
+
+	//Login Behavior
 }
 
 func Connect() {
@@ -52,12 +74,16 @@ func Connect() {
 
 	//Confirmation
 	fmt.Println("Connected")
+
+	//Function Code From: https://go.dev/doc/tutorial/database-access
 }
 
 func main() {
 	Connect()
+	SetUpDatabase()
 }
 */
+
 
 /*
 //SQL Commands
