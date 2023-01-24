1f979b96f6ec6a16d940f0962241c69acea36e8f
Vladimir Alekseev <58440254+valekseev03@users.noreply.github.com>
2023-01-23 20:11:30 -0500

    Added Extra Functionality

--- MySQLStuff.go
+++ MySQLStuff.go
@@ -55,7 +55,12 @@ func SetUpTables(db *sql.DB) {
 	db.Exec("CREATE TABLE IF NOT EXISTS Users (UserID int NOT NULL AUTO_INCREMENT, Username varchar(255) NOT NULL, Password varchar(255) NOT NULL, UNIQUE(Username), PRIMARY KEY(UserID));")
 
 	//Subscriptions
-	//db.Exec("CREATE TABLE IF NOT EXISTS Subscriptions(UserID int NOT NULL, FOREIGN KEY(UserID) REFERENCES Users(UserID));")
+	//db.Exec("CREATE TABLE IF NOT EXISTS Subscriptions(UserID int NOT NULL, FOREIGN KEY(UserID) REFERENCES Users(UserID));") //update for necessary parameters
+}
+
+func ResetTables(db *sql.DB) {
+	db.Exec("DROP TABLE IF EXISTS Users;")
+	db.Exec("DROP TABLE IF EXISTS Subscriptions;")
 }
 
 func CreateNewUser(db *sql.DB, username string, password string) {
@@ -82,8 +87,24 @@ func CreateNewUser(db *sql.DB, username string, password string) {
 	//Test If User Creation Worked
 }
 
-func DeleteUser(db *sql.DB, username string, password string) {
+/*func DeleteUser(db *sql.DB, username string, password string) {
 	result, err := db.Exec("DELETE FROM Users WHERE Username = ? AND Password = ?;", username, password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	numRows, err := result.RowsAffected()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Rows Affected:", numRows)
+}*/
+
+func DeleteUser(db *sql.DB, ID int) {
+	result, err := db.Exec("DELETE FROM Users WHERE UserID = ?;", ID)
 
 	if err != nil {
 		log.Fatal(err)
@@ -98,13 +119,13 @@ func DeleteUser(db *sql.DB, username string, password string) {
 	fmt.Println("Rows Affected:", numRows)
 }
 
-func Login(db *sql.DB, username string, password string) {
+func Login(db *sql.DB, username string, password string) int {
 	//Try To Login
 	rows, err := db.Query("SELECT UserID FROM Users WHERE Username = ? AND Password = ?;", username, password)
 
 	if err != nil {
 		log.Fatal(err)
-		return
+		return -2
 	}
 
 	//Tests If Query Returns Empty Set or Not (Valid Username and Password or Not)
@@ -115,23 +136,28 @@ func Login(db *sql.DB, username string, password string) {
 		rows.Scan(&CurrentUserID)
 		fmt.Println(CurrentUserID)
 
+		return CurrentUserID
 		//Login Behavior
 
 	} else {
 		fmt.Println("Incorrect Username or Password!")
+		return -1
 	}
 }
 
 func main() {
 	//Testing
-	//db := MySQLConnect()
-	//SetUpTables(db)
+	db := MySQLConnect()
+
+	//ResetTables(db)
+	SetUpTables(db)
 
 	//fmt.Println("Users Size:", getTableSize(db, "Users"))
 	//fmt.Println("Subscriptions Size:", getTableSize(db, "Subscriptions"))
 
-	//DeleteUser(db, "test2", "password")
-	//CreateNewUser(db, "test2", "password")
-	//Login(db, "test2", "password")
-}
+	//DeleteUser(db, "root", "password")
+	//DeleteUser(db, 2)
 
+	//CreateNewUser(db, "test", "testing")
+	//Login(db, "test", "testing")
+}
