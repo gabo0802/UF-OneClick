b9b9660c3b9c899799f60399746e94f77088a0d2
gabo0802 <gabriel.castejon@ufl.edu>
2023-01-29 15:05:04 -0500

    Linked Angular to Go

--- api/main.go
+++ api/main.go
@@ -18,20 +18,22 @@ func main() {
 
 	//Angular Connection:
 	r := gin.Default()
-	r.GET("", handler.PingGet("Website", "*insert welcome page*"))
 
-	r.GET("/login", handler.GetLogins)
-	r.GET("/login/:credentials", handler.SetCredentials)
-	//r.POST("/login", handler.PostLogins)
-
-	r.GET("/subscriptions", handler.PingGet("Subscriptions", "*insert all of users subscriptions*"))
-
+	api := r.Group("/api")
+	{
+		api.GET("", handler.PingGet("Website", "*insert welcome page*"))
+		api.GET("/login", handler.GetLogins)
+		api.GET("/login/:credentials", handler.SetCredentials)
+		//r.POST("/login", handler.PostLogins)
+		api.GET("/subscriptions", handler.PingGet("Subscriptions", "*insert all of users subscriptions*"))
+	}
 	/*
-		api := r.Group("/api")	{
-			api.GET("/ping", handler.PingGet())
-		}
+		r.GET("", handler.PingGet("Website", "*insert welcome page*"))
+		r.GET("/login", handler.GetLogins)
+		r.GET("/login/:credentials", handler.SetCredentials)
+		//r.POST("/login", handler.PostLogins)
+		r.GET("/subscriptions", handler.PingGet("Subscriptions", "*insert all of users subscriptions*"))
 	*/
-
 	r.Run("0.0.0.0:5000") //http://127.0.0.1:5000
 
 	fmt.Println("End")
