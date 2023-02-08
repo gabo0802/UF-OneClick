20184dbb4207f547cbdb236acdae2faa7321e72d
matthew <mdenslinger@ufl.edu>
2023-02-07 19:07:44 -0500

    Add ReactiveFormsModule and create reactive form for signup page

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,6 +25,7 @@ import { SignupComponent } from './signup/signup.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialDesignModule
--- Client/src/app/signup/signup.component.ts
+++ Client/src/app/signup/signup.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
 
+  signUpForm: FormGroup = {} as FormGroup;
+
+
+  ngOnInit(){
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'password': new FormControl(null)
+    });
+  }
+
+  onSubmit(){
+    
+  }
 }
