fe19abbc3e61a65d1e2ee43750aa789c5eef3903
enojom <enojom@ufl.edu>
2023-02-08 19:09:11 -0500

    Adds back-end testing function

--- api/main.go
+++ api/main.go
@@ -1,13 +1,84 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"strconv"
 
 	"github.com/gabo0802/UF-OneClick/api/httpd/handler"
 	"github.com/gabo0802/UF-OneClick/api/httpd/handler/MySQL"
 
 	"github.com/gin-gonic/gin"
 )
+
+// UserID will normally be taken automatically from the database
+// instead of specifying it directly, unlike in this test
+func testBackend(db *sql.DB) {
+	fmt.Println("Type -1 to quit the test.")
+	var choice int
+	for choice != -1 {
+		fmt.Print("Enter a number from 1 - 10: ")
+		fmt.Scanln(&choice)
+		if choice == 1 {
+			MySQL.ResetAllTables(db)
+			fmt.Println("Choice 1: Clearing tables from database \"userdb.\"")
+		} else if choice == 2 {
+			MySQL.SetUpTables(db)
+			fmt.Println("Choice 2: Setting up all tables for database \"userdb.\"")
+		} else if choice == 3 {
+			fmt.Println("Choice 3: Getting table sizes.")
+			fmt.Println("Subscription table size: " + strconv.Itoa(MySQL.GetTableSize(db, "subscriptions")))
+			fmt.Println("Users table size: " + strconv.Itoa(MySQL.GetTableSize(db, "users")))
+			fmt.Println("UserSubs table size: " + strconv.Itoa(MySQL.GetTableSize(db, "usersubs")))
+		} else if choice == 4 {
+			fmt.Println("Choice 4: Creates new user.")
+			fmt.Println("Enter a username, password, and email: ")
+			var a, b, c string
+			fmt.Scanln(&a, &b, &c)
+			MySQL.CreateNewUser(db, a, b, c)
+		} else if choice == 5 {
+			fmt.Println("Choice 5: Creates new subscription.")
+			fmt.Println("Enter a name and price: ")
+			var a, b string
+			fmt.Scanln(&a, &b)
+			MySQL.CreateNewSub(db, a, b)
+		} else if choice == 6 {
+			fmt.Println("Choice 6: Subscribes to subscription service.")
+			fmt.Println("Enter a UserID and Subscription Name: ")
+			var a int
+			var b string
+			fmt.Scanln(&a, &b)
+			MySQL.CreateNewUserSub(db, a, b)
+		} else if choice == 7 {
+			fmt.Println("Choice 7: Cancels subscription service.")
+			fmt.Println("Enter a UserID and Subscription Name: ")
+			var a int
+			var b string
+			fmt.Scanln(&a, &b)
+			MySQL.CancelUserSub(db, a, b)
+		} else if choice == 8 {
+			fmt.Println("Choice 8: Resubscribes to subscription service.")
+			fmt.Println("Enter a UserID, Subscription Name, and Date Added: ")
+			var a int
+			var b, c string
+			fmt.Scanln(&a, &b, &c)
+			MySQL.AddOldUserSub(db, a, b, c)
+		} else if choice == 9 {
+			fmt.Println("Choice 9: Deletes user that is specified.")
+			fmt.Println("Enter a UserID: ")
+			var a int
+			fmt.Scanln(&a)
+			MySQL.DeleteUser(db, a)
+		} else if choice == 10 {
+			fmt.Println("Choice 10: Changes the password of specified user.")
+			fmt.Println("Enter a UserID, the Old Password, and the New Password: ")
+			var a int
+			var b, c string
+			fmt.Scanln(&a, &b, &c)
+			MySQL.ChangePassword(db, a, b, c)
+		}
+	}
+}
 
 func main() {
 	//Establishes a connection to the remote MySQL server's database
@@ -16,8 +87,10 @@ func main() {
 	//Defers the closing of the connection to the database until the end of main
 	defer db.Close()
 
+	testBackend(db)
+
 	//MySQL.ResetAllTables(db)
-	MySQL.SetUpTables(db)
+	//MySQL.SetUpTables(db)
 	//MySQL.CreateAdminUser(db)
 
 	//Sets pointer in "handler" package to main.go's db
