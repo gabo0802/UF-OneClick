475a2db2dc41df8aa6a6adca747a57ac74f31c60
matthew <mdenslinger@ufl.edu>
2023-02-10 22:39:56 -0500

    Add error messages and fix one validator on sign up

--- Client/src/app/signup/signup.component.ts
+++ Client/src/app/signup/signup.component.ts
@@ -20,7 +20,7 @@ export class SignupComponent implements OnInit{
   ngOnInit(){
 
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null,[Validators.required, Validators.pattern('[A-z0-9]+')]),
+      'username': new FormControl(null,[Validators.required, Validators.pattern('^[A-z0-9]+$')]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(3)])
     });
