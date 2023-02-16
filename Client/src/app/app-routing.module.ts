8b79d3511e6de748d0fedb393bb89ec0875cf69a
matthew <mdenslinger@ufl.edu>
2023-02-16 13:54:12 -0500

    Add authService and authGuard with basic functionality

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardGuard } from './auth-guard.guard';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent, pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'users', component: DashboardComponent}
+  {path: 'users', canActivate: [AuthGuardGuard], component: DashboardComponent}
   
 ];
 
--- Client/src/app/header/header.component.ts
+++ Client/src/app/header/header.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private http: HttpClient, private router: Router) {};  
 
-  ngOnInit(){
-    if (document.cookie.includes("currentUserID=")){
-        console.log(document.cookie)
-        this.router.navigate(['users']);
-    }
-  }
+export class HeaderComponent {  
 
 }
