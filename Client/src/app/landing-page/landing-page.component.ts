c3f5c66fc414c47b45a86f5310dd4c09881c583e
gabo0802 <gabriel.castejon@ufl.edu>
2023-02-17 17:14:03 -0500

    tried importing material carousel, didn't work, check doc for that

--- Client/src/app/landing-page/landing-page.component.ts
+++ Client/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCarousel, MatCarouselComponent } from '@magloft/material-carousel';
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css',
-              './scroll-bar.scss']
+              './scroll-bar.scss',
+              './visual.scss']
 })
 export class LandingPageComponent {
 
+  slides = [
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'},
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}, 
+    {'image': 'https://gsr.dev/material2-carousel/assets/demo.png'}
+  ];
 }
+
