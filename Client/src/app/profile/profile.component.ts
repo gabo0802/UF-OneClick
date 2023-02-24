2ef90a3a39b364aff19918538083fe1e7a57e7d1
matthew <mdenslinger@ufl.edu>
2023-02-23 21:02:57 -0500

    Add some more styling and some form logic to profile page

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  hide: boolean = true;
+  passwordCharacterLength: number = 3;
+
+  username: string = 'johnny';
+  password: string = '************';
+  email: string = '123@gmail.com';
+
+  usernameForm = new FormGroup({
+    'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
+  });
+
+  emailForm = new FormGroup({
+    'email': new FormControl(this.email, [Validators.email]),
+  });
+
+  passwordForm = new FormGroup({
+    'password': new FormControl(this.password, [Validators.minLength(this.passwordCharacterLength)]),
+  });
 
 }
