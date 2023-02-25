02c805c650a73ce88cb8e9ab874161c499053799
matthew <mdenslinger@ufl.edu>
2023-02-25 12:20:54 -0500

    Update ProfilePage with changes

--- Client/src/app/signup/signup.component.ts
+++ Client/src/app/signup/signup.component.ts
@@ -44,7 +44,7 @@ export class SignupComponent implements OnInit{
       password: this.signUpForm.value.password,
     }
 
-    this.api.createUser(this.signUpForm.value).subscribe(( resultMessage: string[]) => {
+    this.api.createUser(newUser).subscribe(( resultMessage: string[]) => {
     
       this.callDialog(resultMessage[0], resultMessage[1]);
     });
