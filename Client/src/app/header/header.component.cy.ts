26c3974723bde81d06b1eee4791f5b885c6bbc49
matthew <mdenslinger@ufl.edu>
2023-02-27 11:36:39 -0500

    Add some cypress tests to the header component and login component

--- Client/src/app/header/header.component.cy.ts
+++ Client/src/app/header/header.component.cy.ts
@@ -1,13 +1,42 @@
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ApiService } from "../api.service";
+import { AppRoutingModule } from "../app-routing.module";
+import { MaterialDesignModule } from "../material-design/material-design.module";
 import { HeaderComponent } from "./header.component";
 
 describe('HeaderComponent', () => {
     it('mounts', () => {
-      cy.mount(HeaderComponent)
+      cy.mount(HeaderComponent, {
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule],        
+        providers: [ApiService]
+      })
     })
 
-    it('Login and Sign Up button text', () => {
+    it('Login button text', () => {
         cy.mount(HeaderComponent)
-        cy.get('[routerLink="/login"]').should('have.text', 'Login')
-        cy.get('[routerLink="/signup"]').should('have.text', 'Sign Up')
+        cy.get('[routerLink="/login"]').within(() => {
+          cy.get('button').should('have.text', 'Login')})        
+    })
+
+    it('Click login button', ()=>{
+      cy.mount(HeaderComponent)
+      cy.get('[routerLink="/login"]').within( ()=> {
+        cy.get('button').click()
+      })
+    })
+
+    it('Signup button text', () => {
+      cy.mount(HeaderComponent)
+      cy.get('[routerLink="/signup"]').within(() => {
+        cy.get('button').should('have.text', 'Sign Up')})        
+  })
+
+    it('Click Sign up button', ()=>{
+      cy.mount(HeaderComponent)
+      cy.get('[routerLink="/signup"]').within( ()=> {
+        cy.get('button').click()
+      })
     })
   })
--- Client/src/app/login/login.component.cy.ts
+++ Client/src/app/login/login.component.cy.ts
@@ -1,12 +1,21 @@
-import { HttpClient } from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http"
+import { BrowserModule } from "@angular/platform-browser"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { ApiService } from "../api.service"
+import { AppRoutingModule } from "../app-routing.module"
+import { AuthGuard } from "../auth-guard.guard"
+import { AuthService } from "../auth.service"
+import { MaterialDesignModule } from "../material-design/material-design.module"
+import { LoginMessageComponent } from "./login-message/login-message.component"
 import { LoginComponent } from "./login.component"
-import { MatDialog } from '@angular/material/dialog';
 
 
 describe('LoginComponent', () => {
     it('mounts Login', () => {
       cy.mount(LoginComponent, {
-        providers: [{ provide: HttpClient, useValue: null }],
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule],
+        declarations: [LoginMessageComponent],
+        providers: [ApiService, AuthService, AuthGuard]
       })
     })    
   })
