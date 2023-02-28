ab2d48cee04e10f1f74919e636619ea5b09fd456
matthew <mdenslinger@ufl.edu>
2023-02-28 15:45:21 -0500

    Add unit basic test for login component

--- Client/src/app/login/login.component.spec.ts
+++ Client/src/app/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from '../material-design/material-design.module';
 
@@ -32,4 +33,9 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('password input field hidden by default', () => {
+
+    expect(component.hide).toBeTruthy();
+  })
 });
