90d9d93fae964606305d42571f9edb8d4ce6dbba
matthew <mdenslinger@ufl.edu>
2023-03-01 22:37:56 -0500

    Remove login message component

--- Client/src/app/login/login.component.ts
+++ Client/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import { AuthService } from '../auth.service';
