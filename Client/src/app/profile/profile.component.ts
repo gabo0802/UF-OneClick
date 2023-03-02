8a6667676e6706fb4d021f11f5af3f6875ae1d34
matthew <mdenslinger@ufl.edu>
2023-03-02 12:00:34 -0500

    Remove old profile code from profile.component.ts

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -19,21 +19,11 @@ export class ProfileComponent {
   passwordCharacterLength: number = 3;
 
   timezoneEdit: boolean = false;
-  usernameEdit: boolean = false;
-  emailEdit: boolean = false;  
 
-  username: string = 'Loading...';
+  username: string = '';
   password: string = '************';
-  email: string = 'Loading...';
-  timezone: string = 'Loading...';
-
-  usernameForm = new FormGroup({
-    'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
-  });
-
-  emailForm = new FormGroup({
-    'email': new FormControl(this.email, [Validators.email]),
-  });
+  email: string = '';
+  timezone: string = '';
 
   timezoneForm = new FormGroup({
     'timezonedifference': new FormControl(this.timezone, [Validators.pattern('^[-+]{0,1}[0-9][0-9][0-9][0-9]UTC+$')]),
@@ -48,13 +38,6 @@ export class ProfileComponent {
         this.username = resultMessage[0], 
         this.email = resultMessage[1];
 
-        this.usernameForm = new FormGroup({
-          'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
-        });
-      
-        this.emailForm = new FormGroup({
-          'email': new FormControl(this.email, [Validators.email]),
-        });
       });
 
       this.api.getTimezone().subscribe((resultMessage: string[]) => {
@@ -90,85 +73,7 @@ export class ProfileComponent {
     this.timezoneForm = new FormGroup({
       'timezonedifference': new FormControl(this.timezone, [Validators.pattern('^[-+]{0,1}[0-9][0-9][0-9][0-9]UTC+$')]),
     });
-  }
-
-  editUsername(): void {
-    this.usernameEdit = !this.usernameEdit;
-    
-    if (this.usernameEdit == false){
-      var oldUsername: string = this.username
-      this.username = this.usernameForm.controls['username'].value || this.username
-
-      if (this.username != oldUsername){
-        if (confirm("Are you sure you want to change your username from " + oldUsername + " to " + this.username + "?")){
-            if(oldUsername != "root"){
-              this.api.updateUsername(this.username).subscribe((res) => {
-                alert("Username Updated! Logging Out...")
-                this.authService.userLogOut();
-                this.router.navigate(['/login']);
-              })
-            }else{
-              alert("Cannot Change Username of root user!")
-              this.username = oldUsername;
-              
-              this.usernameForm = new FormGroup({
-                'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
-              });
-            }
-        }else{
-          this.username = oldUsername;
-
-          this.usernameForm = new FormGroup({
-            'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
-          });
-        }  
-      }
-    }else{
-      this.usernameForm = new FormGroup({
-        'username': new FormControl(this.username, [Validators.pattern('^[A-z0-9]+$')]),
-      });
-    }
-
-  }
-
-  editEmail(): void {
-    this.emailEdit = !this.emailEdit;
-
-    if (this.emailEdit == false){
-      var oldEmail: string = this.email
-      this.email = this.emailForm.controls['email'].value || this.email
-
-      if (this.email != oldEmail){
-        if (confirm("Are you sure you want to change your email address from " + oldEmail + " to " + this.email + "?")){
-          if(this.username != "root"){
-            this.api.updateUserEmail(this.email).subscribe((res) => {
-              alert("Email Updated! Please Verify Email Before Logging Back In. Logging Out...")
-              this.authService.userLogOut();
-              this.router.navigate(['/login']);
-            })
-          }else{
-            alert("Cannot Change Email Address of root user!")
-
-            this.email = oldEmail;
-
-            this.emailForm = new FormGroup({
-              'email': new FormControl(this.email, [Validators.email]),
-            });
-          }
-        }else{
-          this.email = oldEmail;
-
-          this.emailForm = new FormGroup({
-            'email': new FormControl(this.email, [Validators.email]),
-          });
-        }
-      } 
-    }else{
-      this.emailForm = new FormGroup({
-        'email': new FormControl(this.email, [Validators.email]),
-      });
-    }
-  }
+  }  
 
   editPassword(): void {
     this.callDialog();   
