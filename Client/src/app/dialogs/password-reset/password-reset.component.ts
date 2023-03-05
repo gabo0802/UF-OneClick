adbff212b573a0f9f919cb6b8b1b8abfc31be9bf
matthew <mdenslinger@ufl.edu>
2023-03-05 15:23:44 -0500

    Add dialogs to password-reset component

--- Client/src/app/dialogs/password-reset/password-reset.component.ts
+++ Client/src/app/dialogs/password-reset/password-reset.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { AuthService } from 'src/app/auth.service';
+import { DialogsService } from 'src/app/dialogs.service';
 
 @Component({
   selector: 'app-password-reset',
@@ -7,6 +11,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./password-reset.component.css']
 })
 export class PasswordResetComponent {
+
+  constructor(private api: ApiService, private authService: AuthService, private router: Router, private dialogs: DialogsService) {}
 
   passwordCharacterLength: number = 3;
 
@@ -20,7 +26,25 @@ export class PasswordResetComponent {
   });
 
   onSave(): void {
-    
+
+    let oldPassword: string = this.passwordForm.get('oldPassword')?.value;
+    let newPassword: string = this.passwordForm.get('newPassword')?.value;
+
+    //checks if new password is duplicate of old password
+    if(oldPassword === newPassword){
+
+      this.passwordForm.setErrors({'duplicate': true});
+    }
+    else{
+
+      this.api.updateUserPassword(oldPassword, newPassword).subscribe( (res) => {
+
+        this.dialogs.successDialog("Your Password has been successfully reset. Please log back in.");
+
+        this.authService.userLogOut();
+        this.router.navigate(['login']);
+      });
+    }    
   }
 
 }
