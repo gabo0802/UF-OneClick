67f394037b3ee4a1e0ff996d015fc7682509e5e7
matthew <mdenslinger@ufl.edu>
2023-03-16 20:55:02 -0400

    Add four new unit tests for login component

--- Client/src/app/login/login.component.cy.ts
+++ Client/src/app/login/login.component.cy.ts
@@ -59,4 +59,91 @@ describe('LoginComponent', () => {
       cy.get('[formControlName="username"]').type('Johnny').should('have.value', 'Johnny')
       cy.get('mat-form-field').get('input').get('[formControlName="password"]').type('kittycat').should('have.value', 'kittycat')
     })
+
+    it('Login Button disabled when form is empty', () => {
+
+      cy.mount(LoginComponent, {
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
+        declarations: [],
+        providers: [ApiService, AuthService, AuthGuard, MatDialog, Router]
+      })
+
+      cy.get('mat-card-actions').contains('button', 'Login').then(($btn) => {
+        if ($btn.is(':disabled')) {
+          cy.log('Button exists and is disabled!')
+          return
+        }
+        else {
+          cy.log('Button exists and is enabled!')
+          cy.wrap($btn).click()
+        }
+      })
+    })
+
+    it('Login Button disabled when password form is empty', () => {
+
+      cy.mount(LoginComponent, {
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
+        declarations: [],
+        providers: [ApiService, AuthService, AuthGuard, MatDialog, Router]
+      })
+
+      cy.get('[formControlName="username"]').type('Susie')
+
+      cy.get('mat-card-actions').contains('button', 'Login').then(($btn) => {
+        if ($btn.is(':disabled')) {
+          cy.log('Button exists and is disabled!')
+          return
+        }
+        else {
+          cy.log('Button exists and is enabled!')
+          cy.wrap($btn).click()
+        }
+      })
+    })
+
+    it('Login Button disabled when username form is empty', () => {
+
+      cy.mount(LoginComponent, {
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
+        declarations: [],
+        providers: [ApiService, AuthService, AuthGuard, MatDialog, Router]
+      })
+
+      cy.get('mat-form-field').get('input').get('[formControlName="password"]').type('MyPassword')
+
+      cy.get('mat-card-actions').contains('button', 'Login').then(($btn) => {
+        if ($btn.is(':disabled')) {
+          cy.log('Button exists and is disabled!')
+          return
+        }
+        else {
+          cy.log('Button exists and is enabled!')
+          cy.wrap($btn).click()
+        }
+      })
+    })
+
+    it('Login Button enabled when username and password filled in', () => {
+
+      cy.mount(LoginComponent, {
+        imports: [HttpClientModule, MaterialDesignModule, BrowserAnimationsModule, BrowserModule, AppRoutingModule, ReactiveFormsModule],
+        declarations: [],
+        providers: [ApiService, AuthService, AuthGuard, MatDialog, Router]
+      })
+
+      cy.get('[formControlName="username"]').type('Susie')
+      cy.get('mat-form-field').get('input').get('[formControlName="password"]').type('MyPassword')
+
+      cy.get('mat-card-actions').contains('button', 'Login').then(($btn) => {
+        if ($btn.is(':enabled')) {
+          cy.log('Button exists and is enabled!')
+          return
+        }
+        else {
+          cy.log('Button exists and is disabled!')          
+        }
+      })
+    })
+
   })
