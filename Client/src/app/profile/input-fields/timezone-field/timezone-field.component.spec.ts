fc8424bd7b2be0f5d0e327a09d944722a1a92f24
matthew <mdenslinger@ufl.edu>
2023-03-20 13:51:29 -0400

    Add five jasmine tests for time-zone component

--- Client/src/app/profile/input-fields/timezone-field/timezone-field.component.spec.ts
+++ Client/src/app/profile/input-fields/timezone-field/timezone-field.component.spec.ts
@@ -46,4 +46,51 @@ describe('TimezoneFieldComponent', () => {
   it('Time Zone form should be disabled initially', () => {
     expect(component.timeZoneForm.disabled).toBeTruthy();
   });
+
+  it('editing variable set to true when editTimeZone() initially called', () =>{
+
+    component.editTimeZone();
+
+    expect(component.editing).toBeTruthy();
+  });
+
+  it('when editTimeZone() called form is enabled', () => {
+
+    component.editTimeZone()
+
+    expect(component.timeZoneForm.enabled).toBeTruthy();
+  });
+
+  it('when editTimeZone() called form value is only "UTC"', () => {
+
+    component.editTimeZone()
+
+    expect(component.timeZoneForm.getRawValue()).toEqual('UTC');
+  });
+
+  it('when editing if editTimeZone() called, editing is false and form is disabled', () => {
+
+    //enable editing
+    component.editTimeZone()
+
+    //disabled editing
+    component.editTimeZone()
+
+    expect(component.timeZoneForm.disabled).toBeTruthy();
+    expect(component.editing).toBeFalsy();
+  });
+
+  it('Time Zone form has duplicate error if same Time Zone is entered', () => {
+
+    //No error
+    expect(component.timeZoneForm.hasError('duplicate')).toBeFalsy();
+
+    component.oldTimeZone = '0500UTC';
+    component.timeZoneForm.setValue('0500UTC');
+
+    //triggers error
+    component.updateTimeZone();
+
+    expect(component.timeZoneForm.hasError('duplicate')).toBeTruthy();
+  });
 });
