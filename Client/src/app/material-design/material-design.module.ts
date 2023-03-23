409d1356447070dd9126c4946ac063cb2fe7ee06
matthew <mdenslinger@ufl.edu>
2023-03-23 10:51:36 -0400

    Remove UserDashboard from Material Design Module

--- Client/src/app/material-design/material-design.module.ts
+++ Client/src/app/material-design/material-design.module.ts
@@ -33,7 +33,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatDialogModule,
     ScrollingModule,
     MatTabsModule,
-    UserDashboard,
     MatTableModule,
     MatProgressSpinnerModule
   ]
