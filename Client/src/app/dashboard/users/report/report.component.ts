1ad26b05c77249103d1870cab67e42b0dc405e3d
matthew <mdenslinger@ufl.edu>
2023-04-05 14:15:58 -0400

    Add forkjoin to report component

--- Client/src/app/dashboard/users/report/report.component.ts
+++ Client/src/app/dashboard/users/report/report.component.ts
@@ -1,8 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 import { DialogsService } from 'src/app/dialogs.service';
 import { MatAccordion } from '@angular/material/expansion';
+import { forkJoin } from 'rxjs';
 
 
 @Component({
@@ -11,15 +12,31 @@ import { MatAccordion } from '@angular/material/expansion';
   styleUrls: ['./report.component.css']
 })
 
-export class ReportComponent {
+export class ReportComponent implements OnInit{
 
-@Input() username: string = '';
+  @Input() username: string = '';
 
   @ViewChild(MatAccordion) accordion: MatAccordion;
 
   constructor(private api: ApiService, private dialogs: DialogsService) {
     this.accordion = new MatAccordion()
     this.accordion.openAll()
+  }
+
+  ngOnInit(): void {
+    
+    // forkJoin({
+    //   NameHere: apicallhere,
+    //   AnotherNameHere: apicallhere,
+    // }).subscribe({
+
+    //   next: (res) => {
+    //     //process results here, ex: variable = res.NameHere,
+    //   },
+    //   error: (error: HttpErrorResponse) => {
+    //     //process errors here
+    //   }
+    // });
   }
 
 }
