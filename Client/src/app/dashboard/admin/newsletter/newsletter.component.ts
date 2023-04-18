59b864743b0f999cd3b6394726b27890a91d2faa
matthew <mdenslinger@ufl.edu>
2023-04-18 18:28:00 -0400

    Add a formcontrol for the textarea

--- Client/src/app/dashboard/admin/newsletter/newsletter.component.ts
+++ Client/src/app/dashboard/admin/newsletter/newsletter.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/api.service';
+import { DialogsService } from 'src/app/dialogs.service';
 
 @Component({
   selector: 'app-newsletter',
@@ -8,24 +11,44 @@ import { ApiService } from 'src/app/api.service';
 })
 export class NewsletterComponent {
   
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private dialogs: DialogsService) { }
+
   todayDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
 
-  newsLetter(): void{
-    var message:string = (document.getElementById('newslettermessage') as HTMLInputElement).value;
+  newsletter: FormControl = new FormControl<string>('', [Validators.required]);
 
-    if(message != "Enter Message For Newsletter!"){
-      if(confirm("Message Will Be Sent To All Users!")){
-        this.api.sendNews(message).subscribe( (res) => {
-            alert("Message Sent");
-            (document.getElementById('newslettermessage') as HTMLInputElement).value = "Enter Message For Newsletter!";
+  sendNewsletter(): void{
+    let message: string = this.newsletter.getRawValue();
+    
+    if(message !== ''){
+
+      if(confirm("Message will be sent to all users!")){
+
+        this.api.sendNews(message).subscribe({
+
+          next: (res) => {
+
+            this.dialogs.successDialog("Message sent to all users.");
+            this.newsletter.reset();
+          },
+          error: (error: HttpErrorResponse) => {
+            this.dialogs.errorDialog("Error sending Message", "An error occured please try again later.");
+          }
         })
       }
     }
+    // if(message != "Enter Message For Newsletter!"){
+    //   if(confirm("Message Will Be Sent To All Users!")){
+    //     this.api.sendNews(message).subscribe( (res) => {
+    //         alert("Message Sent");
+    //         (document.getElementById('newslettermessage') as HTMLInputElement).value = "Enter Message For Newsletter!";
+    //     })
+    //   }
+    // }
   }
 
   resetMessage(): void{
-    (document.getElementById('newslettermessage') as HTMLInputElement).value = "Enter Message For Newsletter!";
+    this.newsletter.reset();
   }
 
   remindTime(): void{
