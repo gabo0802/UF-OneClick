03f91087240da7d1a23b1f292480da8cdcc6862f
matthew <mdenslinger@ufl.edu>
2023-04-18 16:10:14 -0400

    Refactor components in admin

--- Client/src/app/dashboard/admin/admin.component.ts
+++ Client/src/app/dashboard/admin/admin.component.ts
@@ -5,38 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
-  todayDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-  displayHome: boolean = true;
-  displayReset: boolean = false;
-  displayNewsletter: boolean = false;
-
-  goHome(): void {
-
-    if(this.displayHome !== true){
-
-      this.displayReset = false;
-      this.displayHome = true;
-      this.displayNewsletter = false;
-
-    }
-  }
-
-  showReset(): void {
-    if(this.displayReset !== true){
-
-      this.displayReset = true;
-      this.displayHome = false;
-      this.displayNewsletter = false;
-    }    
-  }
-
-  showNewsletter(): void {
-    if(this.displayNewsletter !== true){
-
-      this.displayNewsletter = true;
-      this.displayHome = false;
-      this.displayReset = false;
-    }
-  }
+export class AdminComponent {  
+ 
 }
--- Client/src/app/dashboard/admin/entry/entry.component.ts
+++ Client/src/app/dashboard/admin/entry/entry.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class EntryComponent {
 
+  todayDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
 }
